44907b7392b6255109c3e8955ff55694cdf1f033
Reny Alvarado <renyalvarado@gmail.com>
2022-04-21 18:28:29 -0300

    Add sort_squared_array response to console

--- 02_sorted_squared_array.js
+++ 02_sorted_squared_array.js
@@ -9,3 +9,8 @@ function sort_squared_array(number_array) {
 assert.deepEqual(sort_squared_array([1, 2, 3, 5, 6, 8, 9]), [1, 4, 9, 25, 36, 64, 81])
 assert.deepEqual(sort_squared_array([-2, -1]), [1, 4])
 assert.deepEqual(sort_squared_array([-10, -5, 0, 5, 10]), [0, 25, 25, 100, 100])
+
+
+console.log(sort_squared_array([1, 2, 3, 5, 6, 8, 9]));
+console.log(sort_squared_array([-2, -1]));
+console.log(sort_squared_array([-10, -5, 0, 5, 10]));
