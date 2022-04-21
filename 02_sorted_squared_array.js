6caf5b4064d8720ee8d7c8a949ddde81f6793026
Reny Alvarado <renyalvarado@gmail.com>
2022-04-21 17:31:07 -0300

    Create 02_sorted_squared_array.js

---
+++ 02_sorted_squared_array.js
@@ -0,0 +1,11 @@
+const assert = require('assert').strict;
+
+function sort_squared_array(number_array) {
+    let squared_array = number_array.map(n => n ** 2);
+    squared_array.sort((x, y) => x - y);
+    return squared_array;
+}
+
+assert.deepEqual(sort_squared_array([1, 2, 3, 5, 6, 8, 9]), [1, 4, 9, 25, 36, 64, 81])
+assert.deepEqual(sort_squared_array([-2, -1]), [1, 4])
+assert.deepEqual(sort_squared_array([-10, -5, 0, 5, 10]), [0, 25, 25, 100, 100])
